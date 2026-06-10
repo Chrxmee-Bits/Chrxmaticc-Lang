7ccff27a1636a32929bfe96ea6a6e3f419d7ecdc
Chrxmee_Midnightt <8gxqw5b2h5@privaterelay.appleid.com>
2026-06-09 22:14:13 -0400

    Add detector.js for file type detection

---
+++ chrxmaticc/detector.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const fs = require('fs');
+
+const ICON = path.resolve(__dirname, '..', 'chrxmaticc.svg');
+
+const VALID = new Set([
+    '.chrxm', '.chrxg', '.chrxw', '.chrxi', '.chrxs',
+    '.chrxmg', '.chrxmw', '.chrxmi', '.chrxms',
+    '.chrxgw', '.chrxgi', '.chrxgs',
+    '.chrxwi', '.chrxws',
+    '.chrxsi',
+    '.chrxmgw', '.chrxmgi', '.chrxmgs', '.chrxmwi', '.chrxmws',
+    '.chrxgwi', '.chrxgws', '.chrxgsi',
+    '.chrxwsi', '.chrxmsi',
+    '.chrxmgwi', '.chrxmgws', '.chrxmgsi', '.chrxgwsi', '.chrxmwsi',
+    '.chrxmaticc',
+]);
+
+function detect(filepath) {
+    const ext = path.extname(filepath).toLowerCase();
+    if (VALID.has(ext)) {
+        return fs.existsSync(ICON) ? ICON : null;
+    }
+    return null;
+}
+
+module.exports = { detect };
